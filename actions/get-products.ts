ed7e84301564691132b0b8dc43047dcb720571c7
lunaticenslaved <lunaticenslaved@yandex.ru>
2024-01-10 22:24:02 +0300

    feat(category): add category page

--- actions/get-products.ts
+++ actions/get-products.ts
@@ -2,6 +2,8 @@
 
 import queryString from 'query-string';
 
+import { Product } from '@/types';
+
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/products`;
 
 interface Query {
@@ -11,7 +13,12 @@ interface Query {
   isFeatured?: boolean;
 }
 
-export async function getProducts({ categoryId, colorId, sizeId, isFeatured }: Query) {
+export async function getProducts({
+  categoryId,
+  colorId,
+  sizeId,
+  isFeatured,
+}: Query): Promise<Product[]> {
   const url = queryString.stringifyUrl({
     url: URL,
     query: { categoryId, colorId, sizeId, isFeatured },
